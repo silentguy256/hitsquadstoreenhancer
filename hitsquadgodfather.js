c4354603896eb4cc97790d8adf88cd91fbc28f3c
silentguy256 <silentguy@gmail.com>
2023-08-23 13:34:00 +0200

    Cleanup code

--- hitsquadgodfather.js
+++ hitsquadgodfather.js
@@ -14,30 +14,27 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
-/*waitForKeyElements (".item-title", actionFunction);
-
-function actionFunction (jNode) {
-    jNode[0].innerHTML="<a href='https://steamdb.info/search/?a=app&q="+jNode[0].innerText+"&type=1&category=0' target='_blank'>"+jNode[0].innerText+"</a>";
-//    jNode[0].innerHTML="<a href='https://store.steampowered.com/search/?term="+jNode[0].innerText+"' target='_blank'>"+jNode[0].innerText+"</a>";
-}
-*/
-
 var hidden = GM_SuperValue.get ("hit_hidden", []);
 
-waitForKeyElements ("md-card", actionFunction2);
-waitForKeyElements (".side-bar", actionFunction1);
+waitForKeyElements ("md-card", perGameFunction);
+waitForKeyElements (".side-bar", sidebarFunction);
 
-function actionFunction1 (jNode) {
+function sidebarFunction (jNode) {
+// Create Buttons
     var c = document.createElement("button");
     c.className = "md-raised md-primary public-store-buy-button md-button md-dark-theme md-ink-ripple";
     c.innerText="Clear";
     c.onclick = function(){GM_deleteValue("hit_hidden");location.reload()};
     jNode[0].appendChild(c);
+
+    
     c = document.createElement("button");
     c.className = "md-raised md-primary public-store-buy-button md-button md-dark-theme md-ink-ripple";
     c.innerText="Export";
     c.onclick = function(){prompt("Hidden games:",GM_getValue("hit_hidden"))};
     jNode[0].appendChild(c);
+
+    
     c = document.createElement("button");
     c.className = "md-raised md-primary public-store-buy-button md-button md-dark-theme md-ink-ripple";
     c.innerText="Import";
@@ -52,23 +49,26 @@ function actionFunction1 (jNode) {
 
 
 }
-function actionFunction2 (jNode) {
+function perGameFunction (jNode) {
+
     var x = jNode[0].childNodes[7].childNodes[1];
+
+// Create Hide Button    
     var c = document.createElement("button");
     c.className = "md-raised md-primary public-store-buy-button md-button md-dark-theme md-ink-ripple";
     c.innerText="HIDE";
     c.onclick = function(){hideGame(x.innerText);actionFunction2 (jNode)};
-//    console.log(c);
     jNode[0].children[2].appendChild(c);
+
+// Delete Tile if hidden
     if (hidden.includes(x.innerText)) jNode[0].remove();
+
+// Modify title to be link    
     x.innerHTML="<a href='https://steamdb.info/search/?a=app&q="+x.innerText+"&type=1&category=0' target='_blank'>"+x.innerText+"</a>";
-//    jNode[0].innerHTML="<a href='https://store.steampowered.com/search/?term="+jNode[0].innerText+"' target='_blank'>"+jNode[0].innerText+"</a>";
+//    x.innerHTML="<a href='https://store.steampowered.com/search/?term="+x.innerText+"' target='_blank'>"+x.innerText+"</a>";
 }
 
 function hideGame(game){
     hidden.push(game);
     GM_SuperValue.set ("hit_hidden", hidden);
-
-
-
 }
